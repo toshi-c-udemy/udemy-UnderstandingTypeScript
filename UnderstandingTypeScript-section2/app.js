efce54afa207f51e5e469e54f020d8b107f37a5c
Toshihiro Chida <toshi.c.1986+udemy@gmail.com>
2022-06-25 18:39:37 +0900

    【セクション2】25. function 型 と void 型
    On branch udemy-UnderstandingTypeScript-section2
    Changes to be committed:
    	modified:   app.js
    	modified:   app.ts
    	new file:   union-aliases.ts

--- UnderstandingTypeScript-section2/app.js
+++ UnderstandingTypeScript-section2/app.js
@@ -1,22 +1,8 @@
-function combine(input1, input2, resultConversion) {
-    var result;
-    if ((typeof input1 === 'number' && typeof input2 === 'number') ||
-        resultConversion === 'as-number') {
-        result = +input1 + +input2;
-    }
-    else {
-        result = input1.toString() + input2.toString();
-    }
-    return result;
-    // if (resultConversion === 'as-number') {
-    //   return +result;
-    // } else {
-    //   return result.toString();
-    // }
+function add(n1, n2) {
+    return n1 + n2;
 }
-var combineStringAges = combine(30, 26, 'as-number');
-console.log(combineStringAges);
-var combinedStringNames = combine('30', '26', 'as-number');
-console.log(combinedStringNames);
-var combinedNames = combine('Max', 'Anna', 'as-text');
-console.log(combinedNames);
+function printResult(num) {
+    console.log('Result: ' + num);
+}
+printResult(add(5, 12));
+console.log(printResult(add(5, 12)));
--- UnderstandingTypeScript-section2/app.ts
+++ UnderstandingTypeScript-section2/app.ts
@@ -1,33 +1,11 @@
-type Combinable = number | string;
-type ConversionDescriptor = 'as-number' | 'as-text';
-
-function combine(
-  input1: Combinable,
-  input2: Combinable,
-  resultConversion: ConversionDescriptor
-) {
-  let result;
-  if (
-    (typeof input1 === 'number' && typeof input2 === 'number') ||
-    resultConversion === 'as-number'
-  ) {
-    result = +input1 + +input2;
-  } else {
-    result = input1.toString() + input2.toString();
-  }
-  return result;
-  // if (resultConversion === 'as-number') {
-  //   return +result;
-  // } else {
-  //   return result.toString();
-  // }
+function add(n1: number, n2: number) {
+  return n1 + n2;
 }
 
-const combineStringAges = combine(30, 26, 'as-number');
-console.log(combineStringAges);
+function printResult(num: number): void {
+  console.log('Result: ' + num);
+}
 
-const combinedStringNames = combine('30', '26', 'as-number');
-console.log(combinedStringNames);
+printResult(add(5, 12));
 
-const combinedNames = combine('Max', 'Anna', 'as-text');
-console.log(combinedNames);
+console.log(printResult(add(5, 12)));
