df57ed07546771c20375f4ca75553e9b1d3b6b63
Toshihiro Chida <toshi.c.1986+udemy@gmail.com>
2022-06-26 19:22:58 +0900

    【セクション4】52. スプレッドオペレータ（...）
    On branch udemy-UnderstandingTypeScript-section4
    Changes to be committed:
    	modified:   src/app.ts

--- UnderstandingTypeScript-section4/src/app.ts
+++ UnderstandingTypeScript-section4/src/app.ts
@@ -33,3 +33,17 @@ if (button) {
     console.log(event);
   });
 }
+
+const hobbies = ['Sports', 'Cooking'];
+const activeHobbies = ['Hiking', ...hobbies];
+
+activeHobbies.push(...hobbies);
+
+const person = {
+  name: 'Max',
+  age: 30,
+};
+
+const copiedPerson = {
+  ...person,
+};
