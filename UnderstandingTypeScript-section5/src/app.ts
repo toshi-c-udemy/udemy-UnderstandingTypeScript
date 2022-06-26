0f26faeaab13f2c2f24e155128a14b6c0e3b82ec
Toshihiro Chida <toshi.c.1986+udemy@gmail.com>
2022-06-27 02:26:07 +0900

    【セクション5】73. クラスでのインタフェースの実装
    On branch udemy-UnderstandingTypeScript-section5
    Changes to be committed:
    	modified:   src/app.ts

--- UnderstandingTypeScript-section5/src/app.ts
+++ UnderstandingTypeScript-section5/src/app.ts
@@ -1,18 +1,24 @@
-interface Person {
+interface Greetable {
   name: string;
-  age: number;
 
   greet(phrase: string): void;
 }
 
-let user1: Person;
+class Person implements Greetable {
+  name: string;
+  age = 30;
+  constructor(n: string) {
+    this.name = n;
+  }
 
-user1 = {
-  name: 'Max',
-  age: 30,
   greet(phrase: string) {
     console.log(phrase + ' ' + this.name);
-  },
-};
+  }
+}
+
+let user1: Greetable;
+
+user1 = new Person('Max');
 
 user1.greet('Hello I am');
+console.log(user1);
