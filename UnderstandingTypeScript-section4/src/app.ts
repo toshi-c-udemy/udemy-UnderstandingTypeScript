3316c80bb273e7b0607bf63ca61fb327c38cb673
Toshihiro Chida <toshi.c.1986+udemy@gmail.com>
2022-06-26 18:51:11 +0900

    【セクション4】49. "let" & "const"
    On branch udemy-UnderstandingTypeScript-section4
    Changes to be committed:
    	modified:   src/app.ts

--- UnderstandingTypeScript-section4/src/app.ts
+++ UnderstandingTypeScript-section4/src/app.ts
@@ -1,1 +1,19 @@
-// Code goes here!
+const userName = 'Max';
+// userName = 'Maximilian';
+let age = 30;
+
+age = 29;
+
+function add(a: number, b: number) {
+  let result;
+  result = a + b;
+  return result;
+}
+
+// if (age >= 20) {
+//   let isAdult = true;
+// }
+
+// console.log(isAdult);
+
+// console.log(result);
