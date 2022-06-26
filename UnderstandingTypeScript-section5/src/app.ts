80abba9a69f963bfbe687f57243a71f45d91ae5d
Toshihiro Chida <toshi.c.1986+udemy@gmail.com>
2022-06-26 23:56:06 +0900

    【セクション5】65. 継承
    On branch udemy-UnderstandingTypeScript-section5
    Changes to be committed:
    	modified:   src/app.ts

--- UnderstandingTypeScript-section5/src/app.ts
+++ UnderstandingTypeScript-section5/src/app.ts
@@ -24,15 +24,43 @@ class Department {
   }
 }
 
-const accounting = new Department('d1', 'Accounting');
+class ITDepartment extends Department {
+  admins: string[];
+  constructor(id: string, admins: string[]) {
+    super(id, 'IT');
+    this.admins = admins;
+  }
+}
 
-accounting.addEmployee('Max');
-accounting.addEmployee('Manu');
+class AccountingDepartment extends Department {
+  constructor(id: string, private reports: string[]) {
+    super(id, 'Accounting');
+  }
+
+  addReport(text: string) {
+    this.reports.push(text);
+  }
+
+  printReports() {
+    console.log(this.reports);
+  }
+}
+
+const it = new ITDepartment('d1', ['Max']);
+
+it.addEmployee('Max');
+it.addEmployee('Manu');
 
 // accounting.employees[2] = 'Anna';
 
-accounting.describe();
-accounting.printEmployeeInformation();
+it.describe();
+it.printEmployeeInformation();
+
+console.log(it);
+
+const accounting = new AccountingDepartment('d2', []);
+accounting.addReport('Something');
+accounting.printReports();
 
 // const accountingCopy = {
 //   name: 'DUMMY',
