1769840e95c46aaf26d483ad777e7b3d14a6e023
Toshihiro Chida <toshi.c.1986+udemy@gmail.com>
2022-06-26 22:43:44 +0900

    【セクション5】62. "private" & "public" 修飾子
    On branch udemy-UnderstandingTypeScript-section5
    Changes to be committed:
    	modified:   src/app.ts

--- UnderstandingTypeScript-section5/src/app.ts
+++ UnderstandingTypeScript-section5/src/app.ts
@@ -1,5 +1,6 @@
 class Department {
   name: string;
+  private employees: string[] = [];
 
   constructor(n: string) {
     this.name = n;
@@ -8,17 +9,30 @@ class Department {
   describe(this: Department) {
     console.log('Department: ' + this.name);
   }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
 const accounting = new Department('Accounting');
 
-console.log(accounting);
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu');
+
+// accounting.employees[2] = 'Anna';
 
 accounting.describe();
+accounting.printEmployeeInformation();
 
-const accountingCopy = {
-  name: 'DUMMY',
-  describe: accounting.describe,
-};
+// const accountingCopy = {
+//   name: 'DUMMY',
+//   describe: accounting.describe,
+// };
 
-accountingCopy.describe();
+// accountingCopy.describe();
