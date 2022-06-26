9d109b5121c81eb54ad1896aa381c8ad0d4e5d57
Toshihiro Chida <toshi.c.1986+udemy@gmail.com>
2022-06-26 19:02:09 +0900

    【セクション4】50. アロー関数
    On branch udemy-UnderstandingTypeScript-section4
    Changes to be committed:
    	modified:   src/app.ts

--- UnderstandingTypeScript-section4/src/app.ts
+++ UnderstandingTypeScript-section4/src/app.ts
@@ -4,11 +4,11 @@ let age = 30;
 
 age = 29;
 
-function add(a: number, b: number) {
-  let result;
-  result = a + b;
-  return result;
-}
+// function add(a: number, b: number) {
+//   let result;
+//   result = a + b;
+//   return result;
+// }
 
 // if (age >= 20) {
 //   let isAdult = true;
@@ -17,3 +17,19 @@ function add(a: number, b: number) {
 // console.log(isAdult);
 
 // console.log(result);
+
+const add = (a: number, b: number) => a + b;
+
+const printOutput: (output: string | number) => void = (output) => {
+  console.log(output);
+};
+
+printOutput(add(2, 5));
+
+const button = document.querySelector('button');
+
+if (button) {
+  button.addEventListener('click', (event) => {
+    console.log(event);
+  });
+}
