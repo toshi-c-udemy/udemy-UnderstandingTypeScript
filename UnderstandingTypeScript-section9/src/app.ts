b09cfd26ab2b890a9249182005a9ddd1a3de0f18
Toshihiro Chida <toshi.c.1986+udemy@gmail.com>
2022-06-28 07:40:28 +0900

    【セクション9】126. 再利用可能なバリデーション機能の作成
    On branch udemy-UnderstandingTypeScript-section9
    Changes to be committed:
    	modified:   src/app.ts

--- UnderstandingTypeScript-section9/src/app.ts
+++ UnderstandingTypeScript-section9/src/app.ts
@@ -1,3 +1,47 @@
+// validate
+interface Validatable {
+  value: string | number;
+  required?: boolean;
+  minLength?: number;
+  maxLength?: number;
+  min?: number;
+  max?: number;
+}
+
+function validate(validatableInput: Validatable) {
+  let isValid = true;
+  if (validatableInput.required) {
+    isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+  }
+  if (
+    validatableInput.minLength != null &&
+    typeof validatableInput.value === 'string'
+  ) {
+    isValid =
+      isValid && validatableInput.value.length >= validatableInput.minLength;
+  }
+  if (
+    validatableInput.maxLength != null &&
+    typeof validatableInput.value === 'string'
+  ) {
+    isValid =
+      isValid && validatableInput.value.length <= validatableInput.maxLength;
+  }
+  if (
+    validatableInput.min != null &&
+    typeof validatableInput.value === 'number'
+  ) {
+    isValid = isValid && validatableInput.value >= validatableInput.min;
+  }
+  if (
+    validatableInput.max != null &&
+    typeof validatableInput.value === 'number'
+  ) {
+    isValid = isValid && validatableInput.value <= validatableInput.max;
+  }
+  return isValid;
+}
+
 // autobind decorator
 function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
   const originalMethod = descriptor.value;
@@ -18,7 +62,7 @@ class ProjectInput {
   element: HTMLFormElement;
   titleInputElement: HTMLInputElement;
   descriptionInputElement: HTMLInputElement;
-  mandayElement: HTMLInputElement;
+  mandayInputElement: HTMLInputElement;
 
   constructor() {
     this.templateElement = document.getElementById(
@@ -39,7 +83,7 @@ class ProjectInput {
     this.descriptionInputElement = this.element.querySelector(
       '#description'
     ) as HTMLInputElement;
-    this.mandayElement = this.element.querySelector(
+    this.mandayInputElement = this.element.querySelector(
       '#manday'
     ) as HTMLInputElement;
 
@@ -50,11 +94,28 @@ class ProjectInput {
   private gatherUserInput(): [string, string, number] | void {
     const enteredTitle = this.titleInputElement.value;
     const enteredDescription = this.descriptionInputElement.value;
-    const enteredManday = this.mandayElement.value;
+    const enteredManday = this.mandayInputElement.value;
+
+    const titleValidatable: Validatable = {
+      value: enteredTitle,
+      required: true,
+    };
+    const descriptionValidatable: Validatable = {
+      value: enteredDescription,
+      required: true,
+      minLength: 5,
+    };
+    const mandayValidatable: Validatable = {
+      value: +enteredManday,
+      required: true,
+      min: 1,
+      max: 1000,
+    };
+
     if (
-      enteredTitle.trim().length === 0 ||
-      enteredDescription.trim().length === 0 ||
-      enteredManday.trim().length === 0
+      !validate(titleValidatable) ||
+      !validate(descriptionValidatable) ||
+      !validate(mandayValidatable)
     ) {
       alert('入力値が正しくありません。再度お試しください。');
       return;
@@ -66,7 +127,7 @@ class ProjectInput {
   private clearInputs() {
     this.titleInputElement.value = '';
     this.descriptionInputElement.value = '';
-    this.mandayElement.value = '';
+    this.mandayInputElement.value = '';
   }
 
   @autobind
