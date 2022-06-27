7fd57cc4366d93cd63c59c39afa6f56af19b1cd8
Toshihiro Chida <toshi.c.1986+udemy@gmail.com>
2022-06-27 17:15:59 +0900

    【セクション7】96. Generics に制約を追加する
    On branch udemy-UnderstandingTypeScript-section7
    Changes to be committed:
    	modified:   src/app.ts

--- UnderstandingTypeScript-section7/src/app.ts
+++ UnderstandingTypeScript-section7/src/app.ts
@@ -12,16 +12,10 @@
 // });
 
 // function merge<T, U>(objA: T, objB: U) : T & U
-function merge<T, U>(objA: T, objB: U) {
+function merge<T extends object, U extends object>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
 
-const mergedObj = merge<
-  {
-    name: string;
-    hobbies: string[];
-  },
-  { age: number }
->({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
+const mergedObj = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
 const mergedObj2 = merge({ name: 'Max' }, { age: 30 });
-console.log(mergedObj.age);
+console.log(mergedObj);
