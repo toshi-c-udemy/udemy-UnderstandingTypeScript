1713aabb9decaf2bed0187f6f5fc73f3726d11e0
Toshihiro Chida <toshi.c.1986+udemy@gmail.com>
2022-06-27 22:33:55 +0900

    【セクション8】110. アクセサとパラメータのデコレータ
    On branch udemy-UnderstandingTypeScript-section8
    Changes to be committed:
    	modified:   src/app.ts

--- UnderstandingTypeScript-section8/src/app.ts
+++ UnderstandingTypeScript-section8/src/app.ts
@@ -33,3 +33,59 @@ class Person {
 const pers = new Person();
 
 console.log(pers);
+
+// ---
+function Log(target: any, propertyName: string | Symbol) {
+  console.log('Property デコレータ');
+  console.log(target, propertyName);
+}
+
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+  console.log('Accessor デコレータ');
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+function Log3(
+  target: any,
+  name: string | Symbol,
+  descriptor: PropertyDescriptor
+) {
+  console.log('Method デコレータ');
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+function Log4(target: any, name: string | Symbol, position: number) {
+  console.log('Parameter デコレータ');
+  console.log(target);
+  console.log(name);
+  console.log(position);
+}
+
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+
+  @Log2
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
+    } else {
+      throw new Error('不正な価格です - 0以下では設定できません');
+    }
+  }
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  @Log3
+  getPriceTax(@Log4 tax: number) {
+    return this._price * (1 + tax);
+  }
+}
