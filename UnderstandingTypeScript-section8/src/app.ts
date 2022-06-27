d587eded69577b35f8779921067ce1874db7c0a6
Toshihiro Chida <toshi.c.1986+udemy@gmail.com>
2022-06-28 00:57:19 +0900

    【セクション8】114. 例："Autobind" デコレータの作成
    On branch udemy-UnderstandingTypeScript-section8
    Changes to be committed:
    	modified:   index.html
    	modified:   src/app.ts

--- UnderstandingTypeScript-section8/src/app.ts
+++ UnderstandingTypeScript-section8/src/app.ts
@@ -98,3 +98,30 @@ class Product {
 
 const p1 = new Product('Book', 100);
 const p2 = new Product('Book2', 200);
+
+function AutoBind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
+
+class Printer {
+  message = 'クリックしました！';
+
+  @AutoBind
+  showMessage() {
+    console.log(this.message);
+  }
+}
+
+const p = new Printer();
+
+const button = document.querySelector('button')!;
+button.addEventListener('click', p.showMessage);
