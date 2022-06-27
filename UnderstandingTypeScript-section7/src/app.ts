60db9d1f1524c729a2234a94f544249f264a57b5
Toshihiro Chida <toshi.c.1986+udemy@gmail.com>
2022-06-27 17:03:24 +0900

    【セクション7】95. Generic 関数の作成
    On branch udemy-UnderstandingTypeScript-section7
    Changes to be committed:
    	modified:   src/app.ts

--- UnderstandingTypeScript-section7/src/app.ts
+++ UnderstandingTypeScript-section7/src/app.ts
@@ -10,3 +10,18 @@
 // promise.then((data) => {
 //   // data.split(' ');
 // });
+
+// function merge<T, U>(objA: T, objB: U) : T & U
+function merge<T, U>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+const mergedObj = merge<
+  {
+    name: string;
+    hobbies: string[];
+  },
+  { age: number }
+>({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
+const mergedObj2 = merge({ name: 'Max' }, { age: 30 });
+console.log(mergedObj.age);
