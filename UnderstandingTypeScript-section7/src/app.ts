bf31ebc83a235e21a1a7c66e57a45db5bd0816ef
Toshihiro Chida <toshi.c.1986+udemy@gmail.com>
2022-06-27 19:47:08 +0900

    【セクション7】101. Generic型のユーティリティ
    On branch udemy-UnderstandingTypeScript-section7
    Changes to be committed:
    	modified:   src/app.ts

--- UnderstandingTypeScript-section7/src/app.ts
+++ UnderstandingTypeScript-section7/src/app.ts
@@ -76,3 +76,21 @@ const numberStorage = new DataStorage<number>();
 // // ...
 // objStorage.removeItem(obj);
 // console.log(objStorage.getItems());
+
+interface CourseGoal {
+  title: string;
+  description: string;
+  completeUntil: Date;
+}
+
+function createCourseGoal(title: string, description: string, date: Date) {
+  let courseGoal: Partial<CourseGoal> = {};
+  courseGoal.title = title;
+  courseGoal.description = description;
+  courseGoal.completeUntil = date;
+  return courseGoal as CourseGoal;
+}
+
+const names: Readonly<string[]> = ['Max', 'Anna'];
+// names.push('Manu');
+// names.pop()
