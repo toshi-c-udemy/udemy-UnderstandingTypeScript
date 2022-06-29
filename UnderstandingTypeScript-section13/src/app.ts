7a79841a202e2827d687a3acb05f4ab2f89ce244
Toshihiro Chida <toshi.c.1986+udemy@gmail.com>
2022-06-30 05:45:50 +0900

    【セクション13】174. Axios & 住所の座標取得
    On branch udemy-UnderstandingTypeScript-section13
    Changes to be committed:
    	modified:   index.html
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/app.ts

--- UnderstandingTypeScript-section13/src/app.ts
+++ UnderstandingTypeScript-section13/src/app.ts
@@ -10,6 +10,8 @@ type GoogleGeocodingResponse = {
   status: 'OK' | 'ZERO_RESULTS';
 };
 
+// declare var google: any;
+
 function searchAddressHandler(event: Event) {
   event.preventDefault();
   const enteredAddress = addressInput.value;
@@ -26,6 +28,14 @@ function searchAddressHandler(event: Event) {
         throw new Error('座標を取得できませんでした。');
       }
       const coordinates = response.data.results[0].geometry.location;
+      const map = new google.maps.Map(document.getElementById('map')!, {
+        center: coordinates,
+        zoom: 16,
+      });
+      new google.maps.Marker({
+        position: coordinates,
+        map: map,
+      });
     })
     .catch((err) => {
       alert(err.message);
