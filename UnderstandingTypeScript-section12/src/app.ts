cf90d97b2fc8e352c4770d1d93ac7141172af673
Toshihiro Chida <toshi.c.1986+udemy@gmail.com>
2022-06-30 01:49:47 +0900

    【セクション12】166. class-transformerの例
    On branch udemy-UnderstandingTypeScript-section12
    Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/app.ts
    	new file:   src/product.model.ts
    	modified:   tsconfig.json

--- UnderstandingTypeScript-section12/src/app.ts
+++ UnderstandingTypeScript-section12/src/app.ts
@@ -1,7 +1,21 @@
-import _ from 'lodash';
+import 'reflect-metadata';
+import { plainToInstance } from 'class-transformer';
 
-console.log(_.shuffle([1, 2, 3]));
+import { Product } from './product.model';
 
-declare var GLOBAL: any;
+const products = [
+  { title: '商品1', price: 100 },
+  { title: '商品2', price: 200 },
+];
 
-console.log(GLOBAL);
+// const p1 = new Product('商品1', 100);
+
+// const loadedProducts = products.map((prod) => {
+//   return new Product(prod.title, prod.price);
+// });
+
+const loadedProducts = plainToInstance(Product, products);
+
+for (const prod of loadedProducts) {
+  console.log(prod.getInformation());
+}
