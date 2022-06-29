60a431947fcc9c0328281e6369632b9c866493e8
Toshihiro Chida <toshi.c.1986+udemy@gmail.com>
2022-06-29 10:12:57 +0900

    【セクション10】147. インポート & エクスポート構文のバリエーション
    On branch udemy-UnderstandingTypeScript-section10
    Changes to be committed:
    	modified:   src/components/base-component.ts
    	modified:   src/components/project-input.ts
    	modified:   src/components/project-item.ts
    	modified:   src/components/project-list.ts

--- UnderstandingTypeScript-section10/src/components/project-input.ts
+++ UnderstandingTypeScript-section10/src/components/project-input.ts
@@ -1,6 +1,6 @@
-import { Component } from './base-component.js';
-import { Validatable, validate } from '../util/validation.js';
-import { autobind } from '../decorators/autobind.js';
+import Component from './base-component.js';
+import * as Validation from '../util/validation.js';
+import { autobind as Autobind } from '../decorators/autobind.js';
 import { projectState } from '../state/project-state.js';
 
 // ProjectInput Class
@@ -36,25 +36,25 @@ export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
     const enteredDescription = this.descriptionInputElement.value;
     const enteredManday = this.mandayInputElement.value;
 
-    const titleValidatable: Validatable = {
+    const titleValidatable: Validation.Validatable = {
       value: enteredTitle,
       required: true,
     };
-    const descriptionValidatable: Validatable = {
+    const descriptionValidatable: Validation.Validatable = {
       value: enteredDescription,
       required: true,
       minLength: 5,
     };
-    const mandayValidatable: Validatable = {
+    const mandayValidatable: Validation.Validatable = {
       value: +enteredManday,
       required: true,
       min: 1,
       max: 1000,
     };
     if (
-      !validate(titleValidatable) ||
-      !validate(descriptionValidatable) ||
-      !validate(mandayValidatable)
+      !Validation.validate(titleValidatable) ||
+      !Validation.validate(descriptionValidatable) ||
+      !Validation.validate(mandayValidatable)
     ) {
       alert('入力値が正しくありません。再度お試しください。');
       return;
@@ -69,7 +69,7 @@ export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
     this.mandayInputElement.value = '';
   }
 
-  @autobind
+  @Autobind
   private submitHandler(event: Event) {
     event.preventDefault();
     const userInput = this.gatherUserInput();
