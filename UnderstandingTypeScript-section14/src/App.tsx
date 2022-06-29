d5dc9698e891101ccbdab22d8c79fb62a59c9b9a
Toshihiro Chida <toshi.c.1986+udemy@gmail.com>
2022-06-30 08:57:25 +0900

    【セクション14】184. コンポーネント間の連携
    On branch udemy-UnderstandingTypeScript-section14
    Changes to be committed:
    	modified:   src/App.tsx
    	modified:   src/components/NewTodo.tsx

--- UnderstandingTypeScript-section14/src/App.tsx
+++ UnderstandingTypeScript-section14/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import TodoList from './components/TodoList';
 import NewTodo from './components/NewTodo';
 
@@ -8,10 +9,13 @@ import NewTodo from './components/NewTodo';
 
 const App: React.FC = () => {
   const todos = [{ id: 't1', text: 'TypeScriptコースの完了' }];
+  const todoAddHandler = (text: string) => {
+    console.log(text);
+  };
 
   return (
     <div className='App'>
-      <NewTodo />
+      <NewTodo onAddTodo={todoAddHandler} />
       <TodoList items={todos} />
     </div>
   );
